7b61a61ae02c6d5b16be5735f490444d2dffeea3
guopf307 <guopengfei307@126.com>
2025-04-30 16:16:22 +0800

    change file mode to 0666

--- customize.sh
+++ customize.sh
@@ -7,6 +7,3 @@ sed -i 's|#[[:space:]]*WaylandEnable[[:space:]]*=.*|WaylandEnable=false|' /etc/g
 
 chown root:root /etc/udev/rules.d/*
 chmod 0644 /etc/udev/rules.d/*
-id
-udevadm control --reload-rules
-udevadm trigger
