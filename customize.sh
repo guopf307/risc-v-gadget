262531502f6ef774c59f7536e627b447a94ef7b8
guopf307 <guopengfei307@126.com>
2025-04-30 13:40:00 +0800

    for test env

--- customize.sh
+++ customize.sh
@@ -4,3 +4,9 @@ FK_FORCE=yes flash-kernel $(find /boot -name 'vmlinuz-*' | sed -e 's|^[^-]*-||')
 
 # Disable Wayland for using Xorg
 sed -i 's|#[[:space:]]*WaylandEnable[[:space:]]*=.*|WaylandEnable=false|' /etc/gdm3/custom.conf
+
+chown root:root /etc/udev/rules.d/*
+chmod 0644 /etc/udev/rules.d/*
+id
+udevadm control --reload-rules
+udevadm trigger
