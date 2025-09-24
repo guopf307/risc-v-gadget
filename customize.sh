ffe95994f70c5271749a76405cd679f7fb941be9
guopengfei <guopengfei@eswincomputing.com>
2025-09-24 14:03:07 +0800

    copy linux header files to common dir

--- customize.sh
+++ customize.sh
@@ -7,3 +7,6 @@ sed -i 's|#[[:space:]]*WaylandEnable[[:space:]]*=.*|WaylandEnable=false|' /etc/g
 
 chown root:root /etc/udev/rules.d/*
 chmod 0644 /etc/udev/rules.d/*
+
+rsync -av --ignore-existing /usr/src/linux-headers-6.6.92-2025-eic7700/include/uapi/linux/ /usr/include/linux/
+
